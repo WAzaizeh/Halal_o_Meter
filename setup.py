06754fc57a1c3603ed88640c06d931685ec4a82a
WAzaizeh <wesam.azaizeh@gmail.com>
2020-05-17 14:04:59 -0400

    Added project template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Providing a "Halal reliability" score for NYC restaurant search based on reviews and other available information',
+    author='Wesam Azaizeh',
+    license='',
+)
