e42275020d928e664c2bd175d97f2775070f56e4
WAzaizeh <wesam.azaizeh@gmail.com>
2020-05-27 17:29:24 -0400

    Data Collection - Building database functions

--- src/data/data_collection/database_func.py
+++ src/data/data_collection/database_func.py
@@ -1,42 +1,46 @@
 # PostreSQL database for saving the scraped data
 
 import psycopg2
+from psycopg2 import sql
 
-def updateTable(mobileId, price):
+def append_to_businesses(name, id, url, review_count, address):
+    try:
+        connection, cursor = _connect_to_database()
+
+        # insert row into table
+        table = 'Google_businesses' if 'google' in url else 'Yelp_businesses'
+        cursor.execute(                                                # correct
+        sql.SQL("""INSERT INTO {} (Name, ID, URL, Total_review_count, Address) VALUES (%s, %s, %s, %s, %s)""").format(sql.Identifier(table)),
+        (name, id, url, review_count, address))
+
+        # # testing
+        # print("Table After updating record ")
+        # sql_select_query = 'SELECT * from "{}"'.format(table)
+        # cursor.execute(sql_select_query)
+        # record = cursor.fetchall()
+        # print(record)
+
+
+    finally:
+        # closing database connection
+        _close_connection(connection, cursor)
+
+def _connect_to_database():
     try:
         connection = psycopg2.connect(user="wesamazaizeh",
                                       password="melad5RA",
                                       host="127.0.0.1",
                                       port="5432",
                                       database="halal_o_meter")
-
         cursor = connection.cursor()
-
-#        print("Table Before updating record ")
-        sql_select_query = """select * from Yelp_businesses where id = %s"""
-        cursor.execute(sql_select_query, (mobileId, ))
-        record = cursor.fetchone()
-        print(record)
-
-        # Update single record now
-        sql_update_query = """Update mobile set price = %s where id = %s"""
-        cursor.execute(sql_update_query, (price, mobileId))
-        connection.commit()
-        count = cursor.rowcount
-        print(count, "Record Updated successfully ")
-
-        print("Table After updating record ")
-        sql_select_query = """select * from mobile where id = %s"""
-        cursor.execute(sql_select_query, (mobileId,))
-        record = cursor.fetchone()
-        print(record)
-
+        return connection, cursor
     except (Exception, psycopg2.Error) as error:
-        print("Error in update operation", error)
+        print('Error in update operation\n', error)
 
-    finally:
-        # closing database connection.
-        if (connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+
+
+def _close_connection(connection, cursor):
+    if (connection):
+        cursor.close()
+        connection.close()
+        print('PostgreSQL connection is closed')
