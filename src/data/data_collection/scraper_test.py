2196b4f45eb8a54fe483b7a83f1fecc8d1b326e5
Christophe Williams <christophe@cedarlabs.io>
2020-06-06 20:10:36 -0400

    Christophe's changes to run google scraper

--- src/data/data_collection/scraper_test.py
+++ src/data/data_collection/scraper_test.py
@@ -3,8 +3,8 @@ import review_scraper
 google_test_url = 'https://maps.google.com/?cid=4988939335835406517'
 yelp_test_url = 'https://www.yelp.com/biz/mia-halal-food-ozone-park?adjust_creative=xl_eXoprFhB3R9K5U7OxqQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=xl_eXoprFhB3R9K5U7OxqQ'
 
-# data = review_scraper.scrape_google_reviews(google_test_url)
-# print(len(data)) # should be ?
+data = review_scraper.scrape_google_reviews(google_test_url, 4988939335835406517)
+print(len(data)) # should be ?
 
-data = review_scraper.scrape_yelp_reviews(yelp_test_url)
-print(len(data)) # should be 11
+# data = review_scraper.scrape_yelp_reviews(yelp_test_url)
+# print(len(data)) # should be 11
